23ccd3cde12e6ae6d26043dce6f8999ed9951c3b
vertexnormal <vertexnormal@gmail.com>
2015-02-11 20:28:44 -0700

    Work on the C++ based road builder tool.

--- Bin/TerrainEditFilters/testraster.lua
+++ Bin/TerrainEditFilters/testraster.lua
@@ -6,12 +6,26 @@ return
 	Description="Test Raster",
 	options=
 	{
+		{name="Bed width", type="value", value=12},
+		{name="Bed Hardness", type="value", value=0.5},
+		{name="Paving Width", type="value", value=6},
+		{name="Paving Hardness", type="value", value=0.5},
+		{name="Paving Layer", type="value", value=2},
+		{name="Segment steps", type="value", value=10},
 	},
 	
 	execute=function(self)
+		local bedwidth=self.options[1].value
+		local bedhardness=self.options[2].value
+		local pavingwidth=self.options[3].value
+		local pavinghardness=self.options[4].value
+		local pavinglayer=self.options[5].value
+		local segments=self.options[6].value
+		
 		local buffer=RasterBuffer(hmap:GetWidth(), hmap:GetHeight())
 		local blend=RasterBuffer(hmap:GetWidth(), hmap:GetHeight())
-		blend:fill(1)
+		blend:fill(0)
+		
 		local c
 		
 		local plist=RasterVertexList()
@@ -20,22 +34,41 @@ return
 			local norm=WorldToNormalized(hmap,terrain,pos)
 			local hx=math.floor(norm.x*hmap:GetWidth())
 			local hy=math.floor(norm.y*hmap:GetHeight())
-			local ht=GetHeightValue(hmap,hx,hy)
-			plist:push_back(RasterVertex(hx,hy,1))
+			local ht=GetHeightValue(hmap,hx,(hmap:GetHeight()-1)-hy)
+			plist:push_back(RasterVertex(hx,hy,ht))
 		end
 		
 		local curve=RasterVertexList()
-		TessellateLineList(plist, curve, 10)
+		TessellateLineList(plist, curve, segments)
+		
+		-- Build road bed quadlist and rasterize
 		local quad=RasterVertexList()
-		BuildQuadStrip(curve, quad, 10)
-		
+		BuildQuadStrip(curve, quad, bedwidth)
 		RasterizeQuadStrip(buffer, quad)
-		--BlendHeightWithRasterizedBuffer(hmap, buffer, blend)
-		--terrain:ApplyHeightMap()
-		BlendColorWithRasterizedBuffer(blend1, buffer, Color(0,1,0,0))
 		
+		-- Iterate roadbed quadlist and set alternate verts to 0 and 1, rasterize to blend and apply the paving modifier
+		for c=0,quad:size()-1,1 do
+			local v=quad:at(c)
+			v.val_=c % 2
+		end
+		RasterizeQuadStrip(blend, quad)
+		ApplyBedFunction(blend, bedhardness, true)
 		
+		BlendHeightWithRasterizedBuffer(hmap, buffer, blend)
+		terrain:ApplyHeightMap()
 		
+		quad=RasterVertexList()
+		BuildQuadStrip(curve, quad, pavingwidth)
+		blend:fill(0)
+		local color
+		if pavinglayer==0 then color=Color(1,0,0,0)
+		elseif pavinglayer==1 then color=Color(0,1,0,0)
+		elseif pavinglayer==2 then color=Color(0,0,1,0)
+		else color=Color(0,0,0,1)
+		end
+		RasterizeQuadStrip(blend, quad)
+		ApplyBedFunction(blend, pavinghardness, true)
+		BlendColorWithRasterizedBuffer(blend1, blend, color)		
 		blendtex1:SetData(blend1)
 	end,
 }
