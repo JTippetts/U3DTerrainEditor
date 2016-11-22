bd53d9f0ef6be5ca576246b5c359cbd2ffefa860
Carl <carl.douglas@gmail.com>
2016-11-23 00:08:26 +1300

    OBJECT is now URHO3D_OBJECT

--- Urho3DPlayer.h
+++ Urho3DPlayer.h
@@ -29,7 +29,7 @@ using namespace Urho3D;
 /// Urho3DPlayer application runs a script specified on the command line.
 class Urho3DPlayer : public Application
 {
-
+  URHO3D_OBJECT(Urho3DPlayer, Application)
 public:
     /// Construct.
     Urho3DPlayer(Context* context);
